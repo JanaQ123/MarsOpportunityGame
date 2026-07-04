0a300f5c2f86ef30a72ee935ecd1632308aff694
Jana Qudah <jan20220973@std.psut.edu.jo>
2026-07-04 03:39:12 +0300

    more dev

--- Marsyat/Assets/Scripts/PlayerFollow.cs
+++ Marsyat/Assets/Scripts/PlayerFollow.cs
@@ -3,10 +3,19 @@ using UnityEngine;
 public class PlayerFollow : MonoBehaviour
 {
     public Transform target; // drag player/rover here
+    private float offsetX;
+    private float offsetZ;
 
-    void LateUpdate()
+    void Start()
     {
         if (target == null) return;
-        transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+        offsetX = transform.position.x - target.position.x;
+        offsetZ = transform.position.z - target.position.z;
+    }
+
+    void Update()
+    {
+        if (target == null) return;
+        transform.position = new Vector3(target.position.x + offsetX, transform.position.y, target.position.z + offsetZ);
     }
 }
